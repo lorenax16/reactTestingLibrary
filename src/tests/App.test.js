e1bd51664129ba6170c66a98f3efbaf518a1c26f
lorena olivares <lorena.olivares.perez16@gmail.com>
2022-05-10 12:07:46 -0300

    requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,7 +1,55 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
 
-test('Teste se o topo da aplicação contém um conjunto fixo de links de navegação', () => {
-  render(<App />);
+describe('testando o App', () => {
+  it('Teste os textos dos links de navegação', () => {
+    renderWithRouter(<App />);
+    const linkHome = screen.getByRole('link', { name: 'Home' });
+    const linkAbout = screen.getByRole('link', { name: 'About' });
+    const linkFavorite = screen.getByRole('link', { name: 'Favorite Pokémons' });
+
+    expect(linkHome).toBeInTheDocument();
+    expect(linkAbout).toBeInTheDocument();
+    expect(linkFavorite).toBeInTheDocument();
+  });
+
+  it('Teste se a aplicação é redirecionada para a página inicial', () => {
+    const { history } = renderWithRouter(<App />);
+    const linkHome = screen.getByRole('link', { name: 'Home' });
+    userEvent.click(linkHome);
+    // console.log(history);
+    const { pathname } = history.location;
+
+    expect(pathname).toBe('/');
+  });
+
+  it('Teste se a aplicação é redirecionada para a página About', () => {
+    const { history } = renderWithRouter(<App />);
+    const linkAbout = screen.getByRole('link', { name: 'About' });
+    userEvent.click(linkAbout);
+    // console.log(history);
+    const { pathname } = history.location;
+
+    expect(pathname).toBe('/about');
+  });
+
+  it('Teste se a aplicação é redirecionada para a página Favoritos', () => {
+    const { history } = renderWithRouter(<App />);
+    const linkFavorite = screen.getByRole('link', { name: 'Favorite Pokémons' });
+    userEvent.click(linkFavorite);
+    // console.log(history);
+    const { pathname } = history.location;
+
+    expect(pathname).toBe('/favorites');
+  });
+
+  it('Teste se a aplicação é redirecionada para a página Favoritos', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/not-found');
+    const undefine = screen.getByText(/Page requested not found/i);
+    expect(undefine).toBeInTheDocument();
+  });
 });
