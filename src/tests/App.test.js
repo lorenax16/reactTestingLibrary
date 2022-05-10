76353be33bb0733b7102b1668295e54943382553
lorena olivares <lorena.olivares.perez16@gmail.com>
2022-05-10 09:03:47 -0300

    iniciando o projeto, VAMOS COM TUDO

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,7 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('Teste se o topo da aplicação contém um conjunto fixo de links de navegação', () => {
+  render(<App />);
+});
