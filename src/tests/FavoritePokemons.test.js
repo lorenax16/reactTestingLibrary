424fd46a08f134bd088a66d0bfcab660495d9ffd
lorena olivares <lorena.olivares.perez16@gmail.com>
2022-05-10 13:02:02 -0300

    requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import React from 'react';
+import renderWithRouter from '../renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('teste o componente <FavoritePokemons.js />', () => {
+  it('Teste se é exibida na tela a mensagem No favorite pokemon found`', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const mensagem = screen.getByText(/No favorite pokemon found/i);
+    expect(mensagem).toBeInTheDocument();
+  });
+});
